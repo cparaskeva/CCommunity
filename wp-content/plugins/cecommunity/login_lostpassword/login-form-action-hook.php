cd00b6b8b6a25ebf9b532d6b5f999dc2a9a13cd0
Bertrand Couturier <bertrand.couturier@gmail.com>
2014-03-05 10:35:12 +0100

    (half-) fix for "Wordpress logo replaced by the CeComm one" (not a generic solution)

--- wp-content/plugins/cecommunity/login_lostpassword/login-form-action-hook.php
+++ wp-content/plugins/cecommunity/login_lostpassword/login-form-action-hook.php
@@ -9,10 +9,10 @@ $content=ob_get_contents();
 
 // <a href="http://wordpress.org/" title="Powered by WordPress">CECommunity</a>
 // =>
-//<img src="http://localhost/cecommunity/wp-content/uploads/2013/12/logo1.png" alt="The central community project!" title="CECommunity" id="logo-img">
+//<img src="/cecommunity/wp-content/uploads/2013/12/logo1.png" alt="The central community project!" title="CECommunity" id="logo-img">
 
 $content= preg_replace('/<a href="http:\/\/wordpress.org\/" title="Powered by WordPress">CECommunity<\/a>/', 
-'<img src="http://localhost/cecommunity/wp-content/uploads/2013/12/logo1.png" alt="The central community project!" title="CECommunity" id="logo-img">',
+'<img src="/cecommunity/wp-content/uploads/2013/12/logo1.png" alt="The central community project!" title="CECommunity" id="logo-img">',
 $content);
 
 ob_get_clean();
