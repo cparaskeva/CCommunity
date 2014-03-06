bc00c527f16a81baf235627b3d86c6c4b576aad8
Bertrand Couturier <bertrand.couturier@gmail.com>
2014-03-06 08:51:12 +0100

    Draft for the main/home page

--- wp-content/themes/firmasite-child/functions.php
+++ wp-content/themes/firmasite-child/functions.php
@@ -2,9 +2,9 @@
 
 //Set true, enabling debug mode
 if (!defined('DEBUG'))
-    define('DEBUG', true);
+    define('DEBUG', false/*true*/);
 
-define('CECOM_DISABLE_ADMIN_BAR',false);
+define('CECOM_DISABLE_ADMIN_BAR',true/*false*/);
 
 
 /* DO NOT MODIFY THE FOLLOWING FUNCTIONS
--- wp-content/themes/firmasite-child/index.php
+++ wp-content/themes/firmasite-child/index.php
@@ -20,7 +20,51 @@ get_header();
     -->
     
     
-<!-- Include whatever content you want....-->
+    <style>
+    .borders {border:solid 1px #eee;border-radius:10px; margin-bottom:5px}
+    .no-margin-left {margin-left:0}
+    </style>
+    
+	<div class="col-md-12 borders">
+		<h2>Text with links to various main sections</h2>
+		<br><br><br><br><br><br>
+	</div>
+
+	<div class="row no-margin-left">
+		<div class="col-md-6 borders">
+			<h2>News from the influencers</h2>
+			
+			
+			<a class="twitter-timeline" href="https://twitter.com/centralcomm1"  height="300"  data-widget-id="438354056449241088" data-favorites-screen-name="centralcomm1"></a>
+			
+			<a class="twitter-timeline" href="https://twitter.com/centralcomm1"  height="300"  data-widget-id="438354056449241088" data-favorites-screen-name="matteomoci"></a>
+			
+			<a class="twitter-timeline" href="https://twitter.com/centralcomm1"  height="300"  data-widget-id="438354056449241088" data-favorites-screen-name="CybionIT"></a>
+			
+			<a class="twitter-timeline" href="https://twitter.com/centralcomm1"  height="300"  data-widget-id="438354056449241088" data-favorites-screen-name="triverio"></a>
+			
+			 
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+			
+		</div>
+
+		<div class="col-md-6">
+		
+		<div class="borders" style="padding:0 10px">
+			<h2>Last offers</h2>
+			<br><br><br><br><br><br>
+		</div>
+		
+		<div class="borders" style="padding:0 10px">
+			<h2>Last registered organisations</h2>
+			<br><br><br><br><br><br>
+		</div>
+		
+		</div>
+		
+	</div>
 
 
 </div><!-- #primary .content-area -->
