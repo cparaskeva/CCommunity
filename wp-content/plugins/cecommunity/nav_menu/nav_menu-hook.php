f2f4e4dbedf6d7191b167d15db979af366e20f3d
Bertrand Couturier <bertrand.couturier@gmail.com>
2014-04-03 13:38:02 +0200

    JS that modifies a few links

--- wp-content/plugins/cecommunity/nav_menu/nav_menu-hook.php
+++ wp-content/plugins/cecommunity/nav_menu/nav_menu-hook.php
@@ -6,6 +6,9 @@
  */
 
 function jquery_menu_transform() {
+	
+	$current_user = wp_get_current_user();
+	
 ?>
 
 <script type="text/javascript">
@@ -16,6 +19,25 @@ jQuery( document ).ready( function() {
 
 	jQuery("#menu-topbar-right").append( jQuery(".menu-your-profile").remove() );
 	jQuery("#menu-topbar-right").append( jQuery(".menu-about").remove() );
+
+	/* "edit your profile" link */
+	var profile_url = "/cecommunity/members/<?php echo $current_user->user_login; ?>/profile/";
+	jQuery("li.menu-edit-your-profile a").attr('href', profile_url);
+	
+	/* logout link */
+	var logout_url = "<?php echo htmlspecialchars_decode(wp_logout_url()) ?>";
+	jQuery("li.menu-log-out a").attr('href', logout_url);
+
+	/* "publish an offer" page */
+	if (document.URL.indexOf("/publish-an-offer/") > 0) {
+		jQuery("div.entry-content a").each(function() {
+			var url = jQuery(this).attr('href');
+			var url2 = url.replace('\/members\/cecom\/', '\/members\/<?php echo $current_user->user_login; ?>\/');
+			/*console.log(url); 	console.log(url2);*/
+			jQuery(this).attr('href', url2);
+		});
+	}
+	
 });
 
 //-->
