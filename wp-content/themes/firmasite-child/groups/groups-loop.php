505b05ab7fecd49fac1de0f7a8a48624d13fae96
Michele Minno <minno@cybion.it>
2014-06-05 11:40:42 +0200

    Img for country modified

--- wp-content/themes/firmasite-child/groups/groups-loop.php
+++ wp-content/themes/firmasite-child/groups/groups-loop.php
@@ -93,7 +93,7 @@
                     		$admin_name = $adm->user_login;
                     		$admin_email = '<a href="mailto:'.$adm->user_email.'">'.$adm->user_email.'</a>';
                     		
-                    		echo "<p style=\"background-image: url(/cecommunity/wp-content/uploads/2014/03/country.png)\"></p>$country <b>/ Size</b>: $size <b>/ Sectors</b>: $sectors <b>/ Type</b>: $type <b>/ Administrator</b>: $admin_name $admin_email<hr>";
+                    		echo "<img width=\"16\" src=\"/cecommunity/wp-content/uploads/2014/03/country.png)\"></img>$country <b>/ Size</b>: $size <b>/ Sectors</b>: $sectors <b>/ Type</b>: $type <b>/ Administrator</b>: $admin_name $admin_email<hr>";
                     		?>
                     	</span>
                     </div>
